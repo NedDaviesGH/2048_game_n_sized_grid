63e9a64ce349c8a6b13b8b763a7b016db7a327f5
neddavies <ned.davies16@gmail.com>
2026-01-13 13:19:13 +0000

    Changed sidebar and added github build action

--- backend/components/sidebar.py
+++ backend/components/sidebar.py
@@ -6,9 +6,18 @@ def Sidebar():
             P("Menu", cls="menu-label has-text-white is-1 has-text-weight-bold has-text-centered is-size-4"),
             Ul(cls="menu-list")(
                 Li(A("Home", href="/", cls="has-text-white")),
-                Li(A("Profile", href="/profile", cls="has-text-white")),
-                Li(A("Relaxation Gallery", href="/relaxation", cls="has-text-white")),
-                Li(A("2048 Game", href="/choose_size", cls="has-text-white")),
+                Li(A("Profile", href="/profile", cls="has-text-white"))
+            ),
+            P("Random", cls="menu-label has-text-white has-text-weight-bold"),
+            Ul(cls="menu-list")(
+                Li(A("Relaxation Gallery", href="/relaxation", cls="has-text-white"))
+            ),
+            P("Games", cls="menu-label has-text-white has-text-weight-bold"),
+            Ul(cls="menu-list")(
+                Li(A("2048 Game", href="/choose_size", cls="has-text-white"))
+            ),
+            P("Data/Computer Science", cls="menu-label has-text-white has-text-weight-bold"),
+            Ul(cls="menu-list")(
                 Li(A("Friendship Paradox", href="/friends", cls="has-text-white")),
                 Li(A("Binary Search Tree", href="/binary-search-tree", cls="has-text-white"))
             )
